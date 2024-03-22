cdefdbfa1fdbf2ba72b8d46f47ac33dae1569189
walace alves <94018173+WalaceAlves@users.noreply.github.com>
2024-03-22 10:18:41 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let numA = 5;
+
+let numB = 9;
+
+let soma = `A soma de ${numA} e ${numB} é ${numA + numB}`
+
+console.log(soma);
+
+
+let diminuiçao = `A subtração de ${numA} e ${numB} é ${numA - numB}`
+
+console.log(diminuiçao);
+
+
+let multiplicaçao = `A multiplicação de ${numA} e ${numB} é ${numA * numB}`
+
+console.log(multiplicaçao);
+
+
+let divisao = `A divisão de ${numA} e ${numB} é ${numA / numB}`
+
+console.log(divisao);
